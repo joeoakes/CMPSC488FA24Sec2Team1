3feced39fb5f699d5a296f83f4cdfd298f8e066f
KhoaNguyen55 <HoangNKNguyen@proton.me>
2024-10-31 10:34:58 -0400

    wip kinect ros

--- ros/kinect_ros_jazzy/kinect_ros_jazzy/kinect.py
+++ ros/kinect_ros_jazzy/kinect_ros_jazzy/kinect.py
@@ -1,46 +1,35 @@
 #!/usr/bin/env python
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
 import freenect
 import cv2
 import numpy as np
 
-def pretty_depth(depth):
-    np.clip(depth, 0, 2**10 - 1, depth)
-    depth >>= 2
-    depth = depth.astype(np.uint8)
-    return depth
+class ImagePublisher(Node):
+    def __init__(self):
+        super().__init__("kinect_publisher")
+        self.publisher_ = self.create_publisher(Image, 'video_frames', 10)
+          
+        timer_period = 0.1  # seconds
+          
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+             
+        self.br = CvBridge()
 
+    def timer_callback(self):
+        ret = freenect.sync_get_video()
+              
+        if ret != None:
+            frame, timestamp = ret
+            self.publisher_.publish(self.br.cv2_to_imgmsg(frame))
+     
+        self.get_logger().info('Publishing video frame')
 
-def pretty_depth_cv(depth):
-    return pretty_depth(depth)
-
-
-def video_cv(video):
-    return video[:, :, ::-1]  # RGB -> BGR
-
-cv2.namedWindow('Depth')
-cv2.namedWindow('RGB')
-keep_running = True
-
-def display_depth(dev, data, timestamp):
-    global keep_running
-    cv2.imshow('Depth', pretty_depth_cv(data))
-    if cv2.waitKey(10) == 27:
-        keep_running = False
-
-
-def display_rgb(dev, data, timestamp):
-    global keep_running
-    cv2.imshow('RGB', video_cv(data))
-    if cv2.waitKey(10) == 27:
-        keep_running = False
-
-
-def body(*args):
-    if not keep_running:
-        raise freenect.Kill
-
-
-print('Press ESC in window to stop')
-freenect.runloop(depth=display_depth,
-                 video=display_rgb,
-                 body=body)
+def main(args=None):
+  rclpy.init(args=args)
+  image_publisher = ImagePublisher()
+  rclpy.spin(image_publisher)
+  image_publisher.destroy_node()
+  rclpy.shutdown()
