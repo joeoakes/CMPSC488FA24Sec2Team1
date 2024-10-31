799404164303d4d37fc87e6a0953aa45f820bc3f
KhoaNguyen55 <HoangNKNguyen@proton.me>
2024-10-31 15:16:30 -0400

    test camera_info publish

--- ros/kinect_ros_jazzy/kinect_ros_jazzy/publish.py
+++ ros/kinect_ros_jazzy/kinect_ros_jazzy/publish.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
 from cv_bridge import CvBridge
 import freenect
 import cv2
@@ -10,8 +10,9 @@ import numpy as np
 class ImagePublisher(Node):
     def __init__(self):
         super().__init__("kinect_publisher")
-        self.vid_pub = self.create_publisher(Image, '/kinect2/rgb', 10)
+        self.vid_pub = self.create_publisher(Image, '/kinect2/rgb/image', 10)
         self.depth_pub = self.create_publisher(Image, '/kinect2/depth', 10)
+        self.info_pub = self.create_publisher(CameraInfo, '/kinect2/rgb/camera_info', 10)
           
         timer_period = 0.1  # seconds
           
@@ -26,8 +27,12 @@ class ImagePublisher(Node):
         if ret_video != None and ret_depth != None:
             video, _ = ret_video
             depth, _ = ret_depth
+            camera_info = CameraInfo()
+            camera_info.width = 1920
+            camera_info.height = 1080
             self.vid_pub.publish(self.br.cv2_to_imgmsg(video))
             self.depth_pub.publish(self.br.cv2_to_imgmsg(depth))
+            self.info_pub.publish(camera_info)
      
         self.get_logger().info('Publishing video video')
 
