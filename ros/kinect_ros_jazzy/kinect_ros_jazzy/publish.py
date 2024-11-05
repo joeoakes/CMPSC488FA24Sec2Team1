bc904f5aa2f6d890cffd32cebd8a781bd56eb527
KhoaNguyen55 <HoangNKNguyen@proton.me>
2024-11-05 16:35:08 -0500

    fix kinect publishing script

--- ros/kinect_ros_jazzy/kinect_ros_jazzy/publish.py
+++ ros/kinect_ros_jazzy/kinect_ros_jazzy/publish.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import rclpy
+from rclpy.executors import ExternalShutdownException
 from rclpy.node import Node
 from sensor_msgs.msg import Image, CameraInfo
 from cv_bridge import CvBridge
@@ -21,24 +22,47 @@ class ImagePublisher(Node):
         self.br = CvBridge()
 
     def timer_callback(self):
-        ret_video = freenect.sync_get_video()
-        ret_depth = freenect.sync_get_depth()
+        ret_image = self.get_image(freenect.sync_get_video, "rgb8")
+        ret_depth = self.get_image(freenect.sync_get_depth, "mono16")
               
-        if ret_video != None and ret_depth != None:
-            video, _ = ret_video
-            depth, _ = ret_depth
-            camera_info = CameraInfo()
-            camera_info.width = 1920
-            camera_info.height = 1080
-            self.vid_pub.publish(self.br.cv2_to_imgmsg(video))
-            self.depth_pub.publish(self.br.cv2_to_imgmsg(depth))
-            self.info_pub.publish(camera_info)
-     
-        self.get_logger().info('Publishing video video')
+        if ret_image != None and ret_depth != None:
+            self.vid_pub.publish(ret_image)
+            self.depth_pub.publish(ret_depth)
+            self.info_pub.publish(self.get_camera_topic())
+
+    def get_camera_topic(self):
+        camera_info = CameraInfo()
+        camera_info.header.stamp = self.get_clock().now().to_msg()
+        camera_info.width = 640
+        camera_info.height = 480
+        camera_info.distortion_model = "plumb_bob"
+        camera_info.d = [0.012947732047700113, -0.016278227805096242, 0.0020719045565612245, -0.0012254560479249, 0.0]
+        camera_info.k = [362.02061428537024, 0.0, 210.76517637501865, 0.0, 405.17059240104345, 211.97321671296146, 0.0, 0.0, 1.0]
+        camera_info.r = [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0 ,0.0, 1.0]
+        camera_info.p = [361.9730224609375, 0.0, 209.32856710752822, 0.0, 0.0, 405.724365234375, 212.33942579256836, 0.0, 0.0, 0.0, 1.0, 0.0]
+        return camera_info
+
+    def get_image(self, get_function, format):
+        ret_image = get_function()
+        if ret_image != None:
+            image, _ = ret_image
+            image_msg = self.br.cv2_to_imgmsg(image, format)
+            image_msg.header.stamp = self.get_clock().now().to_msg()
+            image_msg.header.frame_id = "kinect_link"
+            return image_msg
+
+        return None
+
 
 def main(args=None):
-  rclpy.init(args=args)
-  image_publisher = ImagePublisher()
-  rclpy.spin(image_publisher)
-  image_publisher.destroy_node()
-  rclpy.shutdown()
+    logger = rclpy.logging.get_logger('logger')
+    rclpy.init(args=args)
+    image_publisher = ImagePublisher()
+    try:
+        logger.info("Started publishing images")
+        rclpy.spin(image_publisher)
+    except (KeyboardInterrupt, ExternalShutdownException):
+        logger.info("Shutting down gracefully")
+        pass
+
+    image_publisher.destroy_node()
