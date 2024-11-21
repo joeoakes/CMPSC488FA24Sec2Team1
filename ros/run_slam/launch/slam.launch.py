a9928d67e4ffd0a1d128eb960bb4a912a9c1574c
KhoaNguyen55 <HoangNKNguyen@proton.me>
2024-11-21 13:35:12 -0500

    fix rtabmap launch files

--- ros/run_slam/launch/slam.launch.py
+++ ros/run_slam/launch/slam.launch.py
@@ -5,33 +5,35 @@ from launch.substitutions import PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='log_to_mongodb',
-            executable='start_log',
-            name='log_to_mongodb'
-        ),
-        Node(
-            package='kinect_ros_jazzy',
-            executable='publish',
-            name='kinect'
-        ),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([
-                PathJoinSubstitution([
-                    FindPackageShare('rtabmap_launch'),
-                    'launch',
-                    'rtabmap.launch.py'
-                ])
-            ]),
-            launch_arguments={
-                'rtabmap_args' : '--delete_db_on_start',
-                'rgb_topic': '/kinect2/rgb/image',
-                'depth_topic': '/kinect2/depth',
-                'camera_info_topic': '/kinect2/rgb/camera_info',
-                'frame_id': 'kinect_link',
-                'approx_sync': 'true',
-            }.items()
-        )
-    ])
+    return LaunchDescription(
+        [
+            Node(
+                package="log_to_mongodb", executable="start_log", name="log_to_mongodb"
+            ),
+            Node(package="kinect_ros_jazzy", executable="publish", name="kinect"),
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    [
+                        PathJoinSubstitution(
+                            [
+                                FindPackageShare("run_slam"),
+                                "rtabmap.launch.py",
+                            ]
+                        )
+                    ]
+                ),
+                launch_arguments={
+                    "args": "--delete_db_on_start",
+                    "rgb_topic": "/kinect2/rgb/image",
+                    "depth_topic": "/kinect2/depth",
+                    "camera_info_topic": "/kinect2/rgb/camera_info",
+                    "frame_id": "kinect_link",
+                    "approx_sync": "true",
+                    "rtabmap_viz": "false",
+                    # "rviz": "true",
+                }.items(),
+            ),
+        ]
+    )
--- ros/run_slam/setup.py
+++ ros/run_slam/setup.py
@@ -1,27 +1,25 @@
 from setuptools import find_packages, setup
 from glob import glob
 
-package_name = 'run_slam'
+package_name = "run_slam"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name, glob('launch/*.launch.py')),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name, glob("launch/*.launch.py")),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='dev',
-    maintainer_email='HoangNKNguyen@proton.me',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="dev",
+    maintainer_email="HoangNKNguyen@proton.me",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-        ],
+        "console_scripts": [],
     },
 )
